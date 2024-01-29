11eab4cea87b88803d46c1aa8199edf1c9decb76
root <root@DESKTOP-LDC68FK>
2024-01-29 20:12:24 -0300

    se arregle zindex de compoente verificationMessage

--- src/components/VerificationMessage/VerificationMessage.jsx
+++ src/components/VerificationMessage/VerificationMessage.jsx
@@ -2,19 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import Alert from "@mui/material/Alert";
 
-const VerificationMessage = () => {
-  const { user } = useAuth();
-
+const VerificationMessage = ({ user, verify }) => {
   const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
-    if (user && !user.reloadUserInfo.emailVerified) {
+    if (user && !verify) {
       setShowMessage(true);
       console.log("entre true");
     } else {
       setShowMessage(false);
     }
-  }, [user]);
+  }, [user, verify]);
 
   return (
     <>
@@ -26,7 +24,6 @@ const VerificationMessage = () => {
             justifyContent: "center",
             width: "100%",
             background: "rgb(255, 245, 138)",
-            zIndex: 99999,
           }}
         >
           Your email is not verified. Please verify your email.
